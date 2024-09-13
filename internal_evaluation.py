144c4fc1109db7f2aa7378a4551267bade0054bb
alankashkash <alan.kashkash@gmail.com>
2024-09-13 12:35:16 +0800

    change of correct answers - Tokyo accounts for seasonal multiplier

--- internal_evaluation.py
+++ internal_evaluation.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def evaluate_answers(data):
     data = data.copy()
-    correct_answers = [1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1]
+    correct_answers = [1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1]
     data['Compliance Binary'] = data['Compliance'].apply(lambda x: 1 if x.strip().lower() == "compliant" else 0)
     compliance_list = data['Compliance Binary'].tolist()
     accuracy_score = sum(compliance_list[i] == correct_answers[i] for i in range(len(correct_answers))) / len(correct_answers)
